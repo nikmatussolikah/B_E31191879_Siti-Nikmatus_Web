b4501a6c5e980145834675f05125b10339ac9326
nikmatussolikah <66730073+nikmatussolikah@users.noreply.github.com>
2021-03-15 13:46:51 +0700

    interface dan polimorfisme dalam php oop

---
+++ minggu1/acara5/latihan/latihan5no1.php
@@ -0,0 +1,74 @@
+<?php
+
+interface hitungLuas {
+	 public function hitungLuasPersegi($sisi);
+
+    public function hitungLuasSegitiga($sisi);
+
+    public function hitungLuasLingkaran($sisi);
+}
+
+class Persegi implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
+    {
+        return $sisi * $sisi;
+    }
+
+    public function hitungLuasSegitiga($sisi)
+    {
+        return "Error : Objek bukanlah segitiga.";
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return "Error : Objek bukanlah Lingkaran";
+    }
+}
+
+class Segitiga implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
+    {
+        return "Error : Objek bukanlah persegi";
+    }
+
+    public function hitungLuasSegitiga($sisi)
+    {
+        return (1/2) * $sisi * $sisi;
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return "Error : Objek bukanlah lingkaran";
+    }
+}
+
+class Lingkaran implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
+    {
+        return "Error : Objek bukanlah persegi";
+    }
+
+    public function hitungLuasSegitiga($sisi)
+    {
+        return "Error : Objek bukanlah segitiga";
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return $sisi * $sisi * pi();
+    }
+}
+
+$objek1 = new Persegi();
+$objek2 = new Segitiga();
+$objek3 = new Lingkaran();
+
+echo "<b>Jika luas sisi = 4</b><hr>";
+echo "Luas Persegi = {$objek1->hitungLuasPersegi(4)}";
+echo '<br>';
+echo "Luas Segitiga = {$objek2->hitungLuasSegitiga(4)}";
+echo '<br>';
+echo "Luas Lingkaran = {$objek3->hitungLuasLingkaran(4)}";
