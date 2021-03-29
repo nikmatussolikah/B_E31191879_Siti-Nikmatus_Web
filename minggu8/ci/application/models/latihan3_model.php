84fbee48ff744c3524a306ecf0f28637263318b3
nikmatussolikah <nikmatussholikhah41@gmail.com>
2021-03-29 11:32:41 +0700

    Model dan Parsing Data

---
+++ minggu8/ci/application/models/latihan3_model.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+  * 
+  */
+ class Hello_model extends Ci_Model
+ {
+ 	//membuat properti dengan nama var $txt
+ 	public $txt = 'Hello World dari CI Model';
+ 	}
+ ?>
---
+++ minggu8/ci/application/views/latihan3view.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Siti Nikmatus Solikah-E31191879</title>
+</head>
+<body>
+	<h2>Hello World dari CI Model</h2>
+	<h3> Menggunakan Controllers,model dan View</h3>
+</body>
+</html>
